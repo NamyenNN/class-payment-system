f6cc3b0a917cd94a3f45746122d56ad59950ee82
NamyenNN <jirut.niwatwong@gmail.com>
2026-07-14 22:46:18 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -6,48 +6,82 @@ async function initLINE() {
             liffId: CONFIG.LIFF_ID
         });
 
+
         if (!liff.isLoggedIn()) {
+
             liff.login();
+
             return;
+
         }
 
+
         const profile = await liff.getProfile();
+
 
         document.getElementById("status").innerHTML =
             "สวัสดี " + profile.displayName;
 
+
+
         const data = {
+
             action: "registerLine",
+
             lineUserID: profile.userId,
+
             displayName: profile.displayName,
+
             pictureUrl: profile.pictureUrl,
+
             studentID: ""
+
         };
 
-        // ส่งข้อมูลไป GAS
+
+
+        // ส่งข้อมูลเข้า GAS
         const result = await postData(data);
 
+
+
         if (result.status !== "success") {
+
             throw new Error(result.message);
+
         }
 
-        // เก็บข้อมูลไว้ใช้ในหน้าถัดไป
-        localStorage.setItem("lineUser", JSON.stringify({
-            lineUserID: profile.userId,
-            displayName: profile.displayName,
-            pictureUrl: profile.pictureUrl,
-            studentID: ""
-        }));
 
-        // ไปหน้า Home
-        window.location.href = "home.html";
+
+        // เก็บข้อมูล LINE
+        localStorage.setItem(
+            "lineUser",
+            JSON.stringify(data)
+        );
+
+
+
+        /*
+            เช็กว่ามีรหัสนักศึกษาหรือยัง
+
+            ตอนนี้ยังไม่มี
+            ให้ไปหน้ากรอกเลข นศ.
+        */
+
+
+        window.location.href = "register.html";
+
+
 
     } catch (err) {
 
+
         console.error(err);
+
 
         document.getElementById("status").innerHTML =
             "❌ " + err.message;
+
 
     }
 
