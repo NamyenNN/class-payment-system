ab8168d80b5121f0c0b88cd9328f92b5e2adb389
NamyenNN <jirut.niwatwong@gmail.com>
2026-07-14 22:16:39 +0700

    Update LIFF_ID in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-    LIFF_ID : "2010708153-9TK3GNM1",
+    LIFF_ID : "2010708153-Qkb2cBGL",
 
     GAS_URL : "https://script.google.com/macros/s/AKfycbwx8oI0ntdKvJ-B3OXUHevpw991q-K0V0KNYSupRUSEZ0dPnX1_ydOkLzIif9qiqPk/exec"
 
