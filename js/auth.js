fa254b9420cc6304668997499f3cdd306edecb7a
NamyenNN <jirut.niwatwong@gmail.com>
2026-07-14 22:01:25 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,55 @@
+async function initLINE(){
 
+  await liff.init({
+    liffId: CONFIG.LIFF_ID
+  });
+
+
+  if(!liff.isLoggedIn()){
+
+    liff.login();
+
+    return;
+
+  }
+
+
+  const profile = await liff.getProfile();
+
+
+
+  const user = {
+
+    action:"registerLine",
+
+    lineUserID: profile.userId,
+
+    displayName: profile.displayName,
+
+    studentID:""
+
+  };
+
+
+
+  await fetch(CONFIG.GAS_URL,{
+
+    method:"POST",
+
+    body:JSON.stringify(user)
+
+  });
+
+
+
+  localStorage.setItem(
+    "lineUser",
+    JSON.stringify(user)
+  );
+
+
+
+  window.location.href="home.html";
+
+
+}
