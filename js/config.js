32b6a435fafc7a47a28a1113d75d7a047dd6c2bc
NamyenNN <jirut.niwatwong@gmail.com>
2026-07-15 00:27:55 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@ const CONFIG = {
 
     LIFF_ID : "2010708153-Qkb2cBGL",
 
-    GAS_URL : "https://script.google.com/macros/s/AKfycbwPWKjW1b_uYBzdQiVK2_4ROFqWok1jM3USeIiLs_U9ft_WIN1UXjds7TD_RIQXYl1A/exec/exec"
+    GAS_URL : "https://script.google.com/macros/s/AKfycbwPWKjW1b_uYBzdQiVK2_4ROFqWok1jM3USeIiLs_U9ft_WIN1UXjds7TD_RIQXYl1A/exec"
 
 };
