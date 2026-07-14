d155916ceea6fc5fcaa95671cef406f9dd6d09a6
NamyenNN <jirut.niwatwong@gmail.com>
2026-07-15 01:08:56 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
+const CONFIG = {
+
+    LIFF_ID : "2010708153-Qkb2cBGL",
+
+    GAS_URL : "https://script.google.com/macros/s/AKfycbwPWKjW1b_uYBzdQiVK2_4ROFqWok1jM3USeIiLs_U9ft_WIN1UXjds7TD_RIQXYl1A/exec"
+
+};
 
