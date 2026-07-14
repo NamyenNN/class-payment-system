e195ad9d2f1edff4772cca40d82db3dd7e3c0654
NamyenNN <jirut.niwatwong@gmail.com>
2026-07-14 23:46:06 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,81 @@
+async function adminLogin(){
 
+
+    const password =
+    document.getElementById("password").value.trim();
+
+
+
+    if(!password){
+
+
+        alert("กรอกรหัสผ่าน");
+
+        return;
+
+    }
+
+
+
+    try{
+
+
+        const res = await fetch(
+
+            CONFIG.GAS_URL +
+
+            "?action=adminLogin&password=" +
+
+            encodeURIComponent(password)
+
+        );
+
+
+
+        const result =
+        await res.json();
+
+
+
+
+        if(result.status === "success"){
+
+
+            localStorage.setItem(
+                "adminLogin",
+                "true"
+            );
+
+
+
+            window.location.href =
+            "admin.html";
+
+
+        }
+        else{
+
+
+            document.getElementById("status").innerText =
+            result.message;
+
+
+        }
+
+
+
+    }
+    catch(err){
+
+
+        console.error(err);
+
+
+        document.getElementById("status").innerText =
+        "เกิดข้อผิดพลาด";
+
+
+    }
+
+
+}
